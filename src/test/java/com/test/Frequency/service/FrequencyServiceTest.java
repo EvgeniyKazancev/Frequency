c686ce310ee2cc16831083349733afe2f35cd0f2
EvgeniyKazancev <ekazancev777@gmail.com>
2023-09-23 19:44:25 +0300

    woop

--- src/test/java/com/test/Frequency/service/FrequencyServiceTest.java
+++ src/test/java/com/test/Frequency/service/FrequencyServiceTest.java
@@ -13,19 +13,18 @@ class FrequencyServiceTest {
 
     private final FrequencyService frequencyService = new FrequencyService();
 
-    @BeforeAll
-    public static void before() {
-        System.out.println("Start test FrequencyService");
-    }
+
+
 
     @Test
     public void testCounter() {
-       LinkedHashMap<Character,Integer> actual = frequencyService.counter("aaabvv");
+       LinkedHashMap<Character,Integer> actual = frequencyService.counter("~!@#$%^&()_+Q{}|A:\"Z<>?Ё!\"№;%:?()_+Й/ФЯ,");
        LinkedHashMap<Character,Integer> expected = new LinkedHashMap<>(){{
            put('a',3);
            put('v',2);
            put('b',1);
        }};
+        System.out.println(expected);
         Assertions.assertEquals(expected,actual);
     }
 }
